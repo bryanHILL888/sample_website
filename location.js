015da6f595f6414bc9717660b5c9b760a4c0bf10
bryanHILL888 <bryanhill@mail.usf.edu>
2016-09-22 13:29:14 -0400

    Initial upload
    This version of my site is old, and will be updated soon.

---
+++ location.js
@@ -0,0 +1,76 @@
+﻿window.onload = getMyLocation; 
+
+function getMyLocation()
+{
+   if (navigator.geolocation)
+   {
+      navigator.geolocation.getCurrentPosition(displayLocation, displayError);
+   }
+   else
+   {
+      alert("It appears as though your browser does not support geolocation.");
+   }
+}
+
+function displayLocation(position)
+{
+   var latitude = position.coords.latitude; 
+   var longitude = position.coords.longitude;  
+   var tampaCoords = 
+   {
+      latitude: 27.950435,
+      longitude: -82.457242
+   };
+   
+   var div = document.getElementById("location");
+   div.innerHTML = "You are at Latitude: " + latitude + ", Longitude: " + longitude; + ", & Altitude: " + position.coords.altitude
+                    + " , accurate to " + position.coords.accuracy + " meters."; 
+                    
+   var km = computeDistance(position.coords, tampaCoords);
+   var distance = document.getElementById("distance");
+   distance.innerHTML = "You are " + km + " km from Tampa, FL";
+   
+   showMap(position.coords);
+}
+
+function displayError(error)
+{
+   
+   var errorTypes = 
+   {
+      0: "Unknown error",
+      1: "Permission denied by user",
+      2: "Position is not available",
+      3: "Request timed out"
+   };
+   var errorMessage = errorTypes[error.code];
+   if (error.code == 0 || error.code == 2)
+   {
+      errorMessage = errorMessage + " " + error.message;
+   }
+   
+   var div = document.getElementById("location");
+   div.innerHTML = errorMessage; 
+}
+
+function computeDistance(startCoords, destCoords)
+{
+   var startLatRads = degreesToRadians(startCoords.latitude);
+   var startLongRads = degreesToRadians(startCoords.longitude);
+   var destLatRads = degreesToRadians(destCoords.latitude);
+   var destLongRads = degreesToRadians(destCoords.longitude);
+   
+   var Radius = 6371; // radius of the Earth in KM
+   var distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) +
+                   Math.cos(startLatRads) * Math.cos(destLatRads) *
+                   Math.cos(startLongRads - destLongRads)) * Radius;
+                   
+   return distance;
+}
+
+function degreesToRadians(degrees)
+{
+   var radians = (degrees * Math.PI)/180;
+   return radians;
+}
+
---
+++ map1.js
@@ -0,0 +1,90 @@
+﻿function detectBrowser() 
+   {
+   var useragent = navigator.userAgent;
+   var mapdiv = document.getElementById("map_canvas");
+    
+   if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) 
+   {
+    mapdiv.style.width = '100%';
+    mapdiv.style.height = '100%';
+   } 
+   else 
+   {
+    mapdiv.style.width = '600px';
+    mapdiv.style.height = '600px';
+   }
+   }
+   
+   window.onload = detectBrowswer();
+   
+   var initialLocation;
+   var browserSupportFlag =  new Boolean();
+   
+   function initialize() 
+   {
+  var myOptions = 
+  {
+    zoom: 18,
+    mapTypeId: google.maps.MapTypeId.SATELLITE,
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+      position: google.maps.ControlPosition.TOP_RIGHT}
+  };
+  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+  map.setTilt(45);
+  map.setHeading(90);
+         
+  // Try W3C Geolocation (Preferred)
+  if(navigator.geolocation) 
+  {
+    browserSupportFlag = true;
+    navigator.geolocation.getCurrentPosition(function(position) 
+    {
+      initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+      map.setCenter(initialLocation);
+    }, function() 
+    {
+      handleNoGeolocation(browserSupportFlag);
+    });
+  // Try Google Gears Geolocation
+  } else if (google.gears) 
+  {
+    browserSupportFlag = true;
+    var geo = google.gears.factory.create('beta.geolocation');
+    geo.getCurrentPosition(function(position) 
+    {
+      initialLocation = new google.maps.LatLng(position.latitude,position.longitude);
+      map.setCenter(initialLocation);
+    }, function() 
+    {
+      handleNoGeoLocation(browserSupportFlag);
+    });
+  // Browser doesn't support Geolocation
+  } else 
+  {
+    browserSupportFlag = false;
+    handleNoGeolocation(browserSupportFlag);
+  }
+  
+  function handleNoGeolocation(errorFlag) 
+  {
+    if (errorFlag == true) 
+    {
+      alert("Geolocation service failed. So I put you in Tampa.");
+      initialLocation = new google.maps.LatLng(27.950767,-82.457049);
+      map.setHeading(90);
+      map.setTilt(45);      
+    } 
+    else 
+    {
+      
+      alert("Geolocation service failed. So I put you in Moscow.");
+      initialLocation = new google.maps.LatLng(55.751218,37.617702);
+      map.setHeading(90);
+      map.setTilt(45);
+    }
+    map.setCenter(initialLocation);
+  }
+ } 
+
